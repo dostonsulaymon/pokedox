00dd32dcc9dbfdae4e8e8c0510cb740ed4ca49a0
dostonsulaymon <dostonqosimiy19@gmail.com>
2025-12-17 12:53:03 +0500

    hi

--- src/pokeapi.ts
+++ src/pokeapi.ts
@@ -1,17 +1,29 @@
+import {Cache} from "./pokecache";
+
 export class PokeAPI {
     private static readonly baseURL = "https://pokeapi.co/api/v2";
+    private cache: Cache;
 
     constructor() {
+        this.cache = new Cache(10000);
     }
-
     async fetchLocations(pageURL?: string | null): Promise<ShallowLocations> {
+        const url = pageURL ?? `${PokeAPI.baseURL}/location-area/`;
 
-        const response = await fetch(pageURL ?? `${PokeAPI.baseURL}/location-area/`);
+        const cached = this.cache.get<ShallowLocations>(url);
+        if (cached) {
+            return cached.val;
+        }
 
+        const response = await fetch(url);
         if(!response.ok) {
             throw new Error(`Error fetching locations: ${response.statusText}`);
         }
-        return await response.json();
+        const data = await response.json();
+
+        this.cache.add(url, data);
+
+        return data;
     }
 
     async fetchLocation(locationName: string): Promise<Location> {
---
+++ src/pokecache.ts
@@ -0,0 +1,45 @@
+import {clearInterval} from "node:timers";
+
+export type CacheEntry<T> = {
+    createdAt: number;
+    val: T;
+
+}
+
+export class Cache {
+    #cache = new Map<string, CacheEntry<any>>();
+    #reapIntervalId: NodeJS.Timeout | undefined = undefined;
+    #interval: number;
+
+    constructor(interval: number){
+        this.#interval = interval;
+        this.#startReapLoop();
+    }
+
+
+    add<T>(key: string, val: T): void {
+        this.#cache.set(key, {createdAt: Date.now(), val});
+    }
+
+    get<T>(key:string){
+        return this.#cache.get(key);
+    }
+
+    #reap(){
+        for (const [key, entry] of this.#cache.entries()) {
+            if(Date.now() - entry.createdAt > this.#interval){
+                this.#cache.delete(key);
+            }
+        }
+    }
+
+    #startReapLoop(){
+        this.#reapIntervalId = setInterval(this.#reap, this.#interval);
+    }
+
+    stopReapLoop(){
+        clearInterval(this.#reapIntervalId);
+        this.#reapIntervalId = undefined;
+    }
+
+}
